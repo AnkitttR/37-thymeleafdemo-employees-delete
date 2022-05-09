732d43b8d56f68f16f45c3e587e823b58a586942
Ankit <ankitthakur1998@gmail.com>
2022-05-10 01:27:25 +0530

    EmployeeService interface & EmployeeServiceImpl class added to service class

--- src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -2,7 +2,7 @@ package com.luv2code.springboot.thymeleafdemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
