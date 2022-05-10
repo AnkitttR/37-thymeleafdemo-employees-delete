a0f656c1c03ba33e7a4e5b9b29b9f33fa0f8b3c9
Ankit <ankitthakur1998@gmail.com>
2022-05-11 00:51:20 +0530

    set employee as a model attribute to pre-populate the form

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -55,10 +55,13 @@ public class EmployeeController {
 	public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
 		
 		// get the employee from the service
+		Employee theEmployee = employeeService.findById(theId);
 		
 		// set employee as a model attribute to pre-populate the form
+		theModel.addAttribute("employee", theEmployee);
 		
 		// send over to our form
+		return "employees/employee-form";
 	}
 	
 	
