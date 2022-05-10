b3fc9ead526c2d1fd02161be0fb878cc753c64ba
Ankit <ankitthakur1998@gmail.com>
2022-05-10 22:49:38 +0530

    Comments added

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -49,8 +50,16 @@ public class EmployeeController {
 		return "employees/employee-form";		
 	}
 	
-	
-	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		
+		// save the employee
+		
+		
+		// use a redirect to prevent duplicate submissions
+		
+		
+	}
 	
 	
 	
