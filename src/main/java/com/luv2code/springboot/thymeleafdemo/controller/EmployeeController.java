e7651d786bb7c7d0073bc06a41f73ba56d914c21
Ankit <ankitthakur1998@gmail.com>
2022-05-10 21:57:24 +0530

    create model attribute to bind form data

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -37,6 +37,14 @@ public class EmployeeController {
 		
 	}
 	
-	
-
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Employee theEmployee = new Employee();
+		
+		
+		return null;
+		
+	}
 }
